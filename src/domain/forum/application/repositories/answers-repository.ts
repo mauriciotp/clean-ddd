909c726fd6577abe6fed46a84fad2986a8860f2c
Mauricio Teixeira Pinto <mauricio.tp_@outlook.com>
2024-07-23 09:09:52 -0300

    feat: implement delete answer use case

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,5 +1,7 @@
 import { Answer } from '../../enterprise/entities/answer'
 
 export interface AnswersRepository {
+  findById(id: string): Promise<Answer | null>
+  delete(answer: Answer): Promise<void>
   create(answer: Answer): Promise<void>
 }
