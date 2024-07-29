aeba249e30c210a4646589c635f801a1aaa8ff30
Mauricio Teixeira Pinto <mauricio.tp_@outlook.com>
2024-07-29 10:59:18 -0300

    feat: implement delete answer comment use case

---
+++ src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
@@ -0,0 +1,42 @@
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
+import { DeleteAnswerCommentUseCase } from './delete-answer-comment'
+import { MakeAnswerComment } from 'test/factories/make-answer-comment'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: DeleteAnswerCommentUseCase
+
+describe('Delete Answer Comment', () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    sut = new DeleteAnswerCommentUseCase(inMemoryAnswerCommentsRepository)
+  })
+
+  it('should be able to delete a answer comment', async () => {
+    const answerComment = MakeAnswerComment()
+
+    inMemoryAnswerCommentsRepository.create(answerComment)
+
+    await sut.execute({
+      answerCommentId: answerComment.id.toString(),
+      authorId: answerComment.authorId.toString(),
+    })
+
+    expect(inMemoryAnswerCommentsRepository.items).toHaveLength(0)
+  })
+
+  it('should NOT be able to delete a answer comment', async () => {
+    const answerComment = MakeAnswerComment({
+      authorId: new UniqueEntityID('author-1'),
+    })
+
+    inMemoryAnswerCommentsRepository.create(answerComment)
+
+    expect(() => {
+      return sut.execute({
+        answerCommentId: answerComment.id.toString(),
+        authorId: 'author-2',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -6,7 +6,25 @@ export class InMemoryAnswerCommentsRepository
 {
   public items: AnswerComment[] = []
 
+  async findById(id: string) {
+    const answerQuestion = this.items.find((item) => item.id.toString() === id)
+
+    if (!answerQuestion) {
+      return null
+    }
+
+    return answerQuestion
+  }
+
   async create(comment: AnswerComment) {
     this.items.push(comment)
+  }
+
+  async delete(answerComment: AnswerComment) {
+    const answerQuestionIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id,
+    )
+
+    this.items.splice(answerQuestionIndex, 1)
   }
 }
